103db77aab3e4b8378c930c3976ef45705a85aeb
Matheus Oliver de Carvalho Cerqueira <escrevaparamatheus@gmail.com>
2024-02-16 13:49:42 +0000

    verificando parágrafos

--- pages/0_2017.py
+++ pages/0_2017.py
@@ -5,8 +5,10 @@ st.write('# 2017 - Primeiro ano de voluntariado')
 st.markdown("""
             Conheci o IEEE neste ano. Já havia visto cartazes dse eventos e cursos espalhados na faculdade, mas nunca tinha me atentado à existência da organização.
             No mesmo ano, um grupo de colegas havia participado de uma competição de robótica da Laureate, mantenedora da UNIFACS à época. Eles ganharam a competição com o robô Amazon-E e fariam uma palestra falando sobre a experiência.
-            Assistindo à palestra, entendi que a organização da palestra era do recém criado capítulo IEEE RAS UNIFACS, atrelado à Sociedade de Robótica e Automação do IEEE. No fim da palestra, o presidente da época disse que haveriam novas atividades em breve das quais uma seria um grupo de estudos de arduino.
-            Passado um tempo, um colega em comum, que ainda não havia entrado no ramo IEEE UNIFACS, me avisou que o capítulo ia marcar um encontro para falar sobre esse grupo de estudos. não havia nada decidido sobre metodologia, então me opfereci para dar uma inrodução sobre o que eu já sabia, pois no semestre de 2017.1 eu havia feito um projeto com arduino para a UFBA, disciplina de Sistemas Microprocesados.
+            Assistindo à palestra, entendi que a organização da palestra era do recém criado capítulo IEEE RAS UNIFACS, atrelado à Sociedade de Robótica e Automação do IEEE. 
+            
+            No fim da palestra, o presidente da época disse que haveriam novas atividades em breve das quais uma seria um grupo de estudos de arduino.
+            Na altura do segundo semestre, um colega em comum, que ainda não havia entrado no ramo IEEE UNIFACS, me avisou que o capítulo ia marcar um encontro para falar sobre esse grupo de estudos. não havia nada decidido sobre metodologia, então me opfereci para dar uma inrodução sobre o que eu já sabia, pois no semestre de 2017.1 eu havia feito um projeto com arduino para a UFBA, disciplina de Sistemas Microprocesados.
             Preparei um material explicativo, montei uma dinâmica, separei o laboratório e pedi aos técnicos os equipamentos necessários para eu ministrar a apresentação para o grupo. A partir de então, pasei a ser responsável por coordenar o projeto do grupo de estudos e dei a ele o nome de "RASduino".
 
 """)
