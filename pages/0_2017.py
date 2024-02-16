60becfa63b86fbbd628ed7d9933043106e863f40
Matheus Oliver de Carvalho Cerqueira <escrevaparamatheus@gmail.com>
2024-02-16 15:04:45 +0000

    Planejando novas ações

--- pages/0_2017.py
+++ pages/0_2017.py
@@ -6,9 +6,19 @@ st.markdown("""
             Conheci o IEEE neste ano. Já havia visto cartazes dse eventos e cursos espalhados na faculdade, mas nunca tinha me atentado à existência da organização.
             No mesmo ano, um grupo de colegas havia participado de uma competição de robótica da Laureate, mantenedora da UNIFACS à época. Eles ganharam a competição com o robô Amazon-E e fariam uma palestra falando sobre a experiência.
             Assistindo à palestra, entendi que a organização da palestra era do recém criado capítulo IEEE RAS UNIFACS, atrelado à Sociedade de Robótica e Automação do IEEE. 
-            
+""")
+
+st.warning('Adicionar foto do workshop Amazon-E')
+
+st.markdown("""
             No fim da palestra, o presidente da época disse que haveriam novas atividades em breve das quais uma seria um grupo de estudos de arduino.
             Na altura do segundo semestre, um colega em comum, que ainda não havia entrado no ramo IEEE UNIFACS, me avisou que o capítulo ia marcar um encontro para falar sobre esse grupo de estudos. não havia nada decidido sobre metodologia, então me opfereci para dar uma inrodução sobre o que eu já sabia, pois no semestre de 2017.1 eu havia feito um projeto com arduino para a UFBA, disciplina de Sistemas Microprocesados.
             Preparei um material explicativo, montei uma dinâmica, separei o laboratório e pedi aos técnicos os equipamentos necessários para eu ministrar a apresentação para o grupo. A partir de então, pasei a ser responsável por coordenar o projeto do grupo de estudos e dei a ele o nome de "RASduino".
 
 """)
+
+st.warning('Adicionar foto da reunião do Rasduino')
+ 
+st.write('## Coordenador do Rasduino')
+
+st.warning('Adicionar texto e fotos com captions das reuniões e projetos do Rasduino')
